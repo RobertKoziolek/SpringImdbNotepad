89f45d4a01d2384379e199d783318c7f5fe8044e
Robert <robertkoziolek4c@gmail.com>
2019-03-24 18:47:26 +0100

    Configured for standalone tomcat deploy

--- src/main/java/com/robcio/imdbNotepad/ImdbNotepadApplication.java
+++ src/main/java/com/robcio/imdbNotepad/ImdbNotepadApplication.java
@@ -2,12 +2,17 @@ package com.robcio.imdbNotepad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
-public class ImdbNotepadApplication {
-
+public class ImdbNotepadApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ImdbNotepadApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ImdbNotepadApplication.class, args);
 	}
---
+++ src/main/java/com/robcio/imdbNotepad/controller/DefaultViewController.java
@@ -0,0 +1,15 @@
+package com.robcio.imdbNotepad.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class DefaultViewController {
+
+    @GetMapping("/")
+    public String showDefaultView() {
+        return "redirect:/tiles";
+    }
+}
